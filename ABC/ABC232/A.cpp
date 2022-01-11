29b67bfae2225e2f04b797f81f7b0577ebd012e0
Utsubo256 <greeeentea256@gmail.com>
2022-01-11 21:16:26 +0900

    Add ABC/ABC232/A.cpp

---
+++ ABC/ABC232/A.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+  int a, b, result = 0, num1 = s[0] - '0', num2 = s[2] - '0';
+  
+  cout << num1 * num2 << endl;
+}
